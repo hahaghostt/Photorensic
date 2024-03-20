e8c82636b58e418210060aabcc30961d0245d18a
ghost <kbinas2022@elam.co.uk>
2024-03-20 12:45:03 +0000

    Photorensic Open Door

--- Assets/Scripts/DoorInteract.cs
+++ Assets/Scripts/DoorInteract.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class DoorInteract : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Animation")]
+    [SerializeField] public Animator OpenDoor;
+    [SerializeField] public Animator CloseDoor; 
+
+    [Header("UI Interact")]
+    [SerializeField] private GameObject InteractE;
+
+    [Header("Door")]
+    [SerializeField] public GameObject Door; 
+
+    private void Start()
     {
-        
+        InteractE.SetActive(false);
+        Animator animator = Door.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void OnTriggerEnter (Collider other) 
     {
-        
+        Debug.Log("EnterDoor"); 
+        InteractE.SetActive(true); 
+
+        if(Input.GetKey(KeyCode.E)) 
+        {
+            OpenDoor.Play("OpenDoor");
+            Debug.Log("AnimationDoorPlay"); 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InteractE.SetActive(false);
+        Debug.Log("LeaveDoor"); 
     }
 }
